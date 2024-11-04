cee0100b8df77b7901efab3df5ba460eb106e26b
xd0904 <ysh080904@gmail.com>
2024-11-04 22:54:33 +0900

    SoundManage

---
+++ Assets/Scirpts/AudioManager.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    [Header("#BGM")]
+    public AudioClip bgmClip;
+    public float bgmVolume;
+    AudioSource bgmPlayer;
+    AudioHighPassFilter bgmEffect;
+
+    [Header("#SFX")]
+    public AudioClip[] sfxClips;
+    public float sfxVolume;
+    public int channels;
+    AudioSource[] sfxPlayers;
+    int channelIndex;
+
+    public enum Sfx { };
+
+    void Awake()
+    {
+        instance = this;
+        Init();
+    }
+
+    void Init()
+    {
+        // 배경음 플레이어 초기화
+        GameObject bgmObject = new GameObject("BgmPlayer");
+        bgmObject.transform.parent = transform;
+        bgmPlayer = bgmObject.AddComponent<AudioSource>();
+        bgmPlayer.playOnAwake = false;
+        bgmPlayer.loop = true;
+        bgmPlayer.volume = bgmVolume;
+        bgmPlayer.clip = bgmClip;
+        bgmEffect = Camera.main.GetComponent<AudioHighPassFilter>();
+
+        // 효과음 플레이어 초기화
+        GameObject sfxObject = new GameObject("SfxPlayer");
+        sfxObject.transform.parent = transform;
+        sfxPlayers = new AudioSource[channels];
+
+        for (int index = 0; index < sfxPlayers.Length; index++)
+        {
+            sfxPlayers[index] = sfxObject.AddComponent<AudioSource>();
+            sfxPlayers[index].playOnAwake = false;
+            sfxPlayers[index].bypassListenerEffects = true;
+            sfxPlayers[index].volume = sfxVolume;
+        }
+    }
+
+    public void PlayBgm(bool isPlay)
+    {
+        if (isPlay)
+        {
+            bgmPlayer.Play();
+        }
+        else 
+        { 
+            bgmPlayer.Stop();
+        }
+    }
+
+    public void EffectBgm(bool isPlay)
+    {
+        bgmEffect.enabled = isPlay;
+    }
+
+    public void PlaySfx(Sfx sfx)
+    {
+        for (int index = 0; index < sfxPlayers.Length; index++)
+        {
+            int loopIndex = (index + channelIndex) % sfxPlayers.Length;
+
+            if (sfxPlayers[loopIndex].isPlaying) continue;
+
+            int ranIndex = 0;
+
+            // 혹시 나중에 효과음이 2개 이상인건 이렇게 하셈
+            /* if (sfx == Sfx.a)
+            {
+                ranIndex = Random.Range(0, 2);
+            } */
+
+            channelIndex = loopIndex;
+            sfxPlayers[loopIndex].clip = sfxClips[(int)sfx + ranIndex];
+            sfxPlayers[loopIndex].Play();
+            break;
+        }
+    }
+}
+
+// 브금은 AudioManager.instance.PlayBgm(true || false)로 시작과 종료에 넣으면 됨
+// Audio HighPass Filter 쓸거면 AudioManager.instance.EffectBgm(true || false)로 적으면 됨
+// 다른 곳에서 효과음 불러올때 AudioManager.instance.PlaySfx(AudioManager.Sfx.뭐뭐); 이런식으로 하면 됨
